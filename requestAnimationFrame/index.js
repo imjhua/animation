335088b19d5469c0d30bb5b7619a13d0a4166201
jihyekim <imjhua@gmail.com>
2020-09-02 17:46:53 +0900

    어딘가를 클릭하면 멈추도록.

--- requestAnimationFrame/index.js
+++ requestAnimationFrame/index.js
@@ -1,15 +1,21 @@
 const img = document.getElementById("img");
 const target = document.getElementById("target");
 let yPos = 0;
+let rafid;
 function render() {
   target.innerHTML = yPos;
   yPos += 10;
 
   img.style.transform = `translateY(${-yPos}px)`;
 
+  // 어딘가를 클릭하면 멈추도록...
   if (yPos < innerHeight) {
-    requestAnimationFrame(render);
+    rafid = requestAnimationFrame(render);
   }
 }
 
 render();
+
+window.addEventListener("click", function () {
+  cancelAnimationFrame(rafid);
+});
