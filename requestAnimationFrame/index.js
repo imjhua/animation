90c7a3fa060bfbca2500f983ee825cc3aaffc6d5
jihyekim <imjhua@gmail.com>
2020-09-03 10:40:17 +0900

    requestAnimationFrame & animation 비교 코드추가

--- requestAnimationFrame/index.js
+++ requestAnimationFrame/index.js
@@ -1,21 +1,61 @@
-const img = document.getElementById("img");
-const target = document.getElementById("target");
-let yPos = 0;
-let rafid;
-function render() {
-  target.innerHTML = yPos;
-  yPos += 10;
+// requestAnimationFrame
+function test1() {
+  const id = "#requestAnimationFrame";
+  const target = document.querySelector(`${id}`);
+  const img = document.querySelector(`${id} .img`);
+  const button = document.querySelector(`${id} button`);
 
-  img.style.transform = `translateY(${-yPos}px)`;
+  let yPos = 0;
+  let rafid;
+  function render() {
+    button.innerHTML = yPos;
+    yPos += 1;
 
-  // 어딘가를 클릭하면 멈추도록...
-  if (yPos < innerHeight) {
-    rafid = requestAnimationFrame(render);
+    img.style.transform = `translateY(${-yPos}px)`;
+
+    // 어딘가를 클릭하면 멈추도록...
+    if (yPos < innerHeight) {
+      console.log(yPos);
+      rafid = requestAnimationFrame(render);
+    }
   }
+
+  button.addEventListener("click", function (e) {
+    // e.preventDefault();
+    e.stopPropagation();
+    // e.stopImmediatePropagation();
+    render();
+  });
+
+  target.addEventListener("click", function () {
+    cancelAnimationFrame(rafid);
+  });
 }
 
-render();
+// animation
+function test2() {
+  const id = "#animation";
+  const target = document.querySelector(`${id}`);
+  const img = document.querySelector(`${id} .img`);
+  const button = document.querySelector(`${id} button`);
 
-window.addEventListener("click", function () {
-  cancelAnimationFrame(rafid);
-});
+  button.addEventListener("click", function (e) {
+    // e.preventDefault();
+    e.stopPropagation();
+    // e.stopImmediatePropagation();
+
+    if (!img.classList.contains("move")) {
+      img.classList.add("move");
+    }
+    if (img.classList.contains("stop")) {
+      img.classList.remove("stop");
+    }
+  });
+
+  target.addEventListener("click", function () {
+    img.classList.add("stop");
+  });
+}
+
+test1();
+test2();
