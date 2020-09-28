3a582bb1b3d59726d7728c474e77d5f5a075c18b
jihyekim <imjhua@gmail.com>
2020-09-28 23:36:16 +0900

    add

--- scrollIntoView/index.js
+++ scrollIntoView/index.js
@@ -1,9 +1,11 @@
 /* 
-Element.scrollIntoView(alignToTop : boolean)메소드를 사용하면,
-스크롤 가능한 요소의 스크롤을 부모 요소의 표시영역 기준으로 이동시킬 수 있.
+옵션을 통해 기준점을 맞출 수 있다.
+- behavior: 전환에니메이션 auto, smooth
+- block: 수직 정렬을 정의 start, center, end, nearest
+- inline: 수평 정렬을 정함 start, center, end, nearest
 */
 
 function myFunction() {
-  var elmnt = document.getElementById("content");
-  elmnt.scrollIntoView();
+  var elmnt = document.getElementById("inner");
+  elmnt.scrollIntoView({ block: "end" });
 }
