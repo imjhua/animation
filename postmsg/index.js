4af6e2edf58eebf17b0d125effc8c9c331d79f09
jihyekim <imjhua@gmail.com>
2020-09-28 14:15:39 +0900

    add

--- postmsg/index.js
+++ postmsg/index.js
@@ -13,6 +13,7 @@ const openPopup = () => {
   popup1 = openWindowPopup("/test");
   // test자식 페이지에서는 window.opener.postMessage('test', '*');
   // 이때 IE에서 전송하는 opener가 정상동작하지 않는다.
+  // Window 인터페이스의 opener 속성은 open()을 사용해 현재 창을 열었던 창의 참조를 반환합니다.
 
   // opener인경우 window
   function receiveMessage1(e) {
@@ -34,6 +35,10 @@ const openPopupByIframe = () => {
                 </html>`;
   // test자식 페이지에서는 window.parent.postMessage('test', '*');
   // 이때 IE에서 전송하는 message는 obj파싱되지 않는 제약이 있다.
+  // Window.parent속성 현재 윈도우 또는 서브 프레임의 상위에 대한 참조이다.
+  // 윈도우에 부모가없는 경우 해당 parent속성은 자체에 대한 참조입니다.
+  // 윈도우가 넣었을 경우 <iframe>, <object>또는 <frame>, 그 부모 윈도우 매립 요소 창이다.
+  
   popup2.document.write(html);
 
   // parent인 경우 반환받은 win
