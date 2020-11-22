e10cd866b7a60652c6b0c97e4412e7cc6b6dbab0
jihyekim <imjhua@gmail.com>
2020-11-22 19:09:23 +0900

    add

--- tab-aria/index.js
+++ tab-aria/index.js
@@ -1,59 +1,31 @@
-const tabs = document.querySelectorAll('[role="tab"]');
-const panels = document.querySelectorAll('[role="tabpanel"]');
+const action = document.querySelector(".action");
 
-for (let i = 0; i < tabs.length; ++i) {
-  addListeners(i);
-}
+action.addEventListener("click", clickEventListener);
 
-function addListeners(index) {
-  tabs[index].addEventListener("click", clickEventListener);
-
-  // Build an array with all tabs (<button>s) in it
-  // tabs[index].index = index;
-}
-
-// When a tab is clicked, activateTab is fired to activate it
+const input = document.querySelector("input");
 function clickEventListener(event) {
-  var tab = event.target;
-  activateTab(tab, false);
-}
-
-// Activates any given tab panel
-function activateTab(tab, setFocus) {
-  setFocus = setFocus || true;
-  // Deactivate all other tabs
-  deactivateTabs();
-
-  // Remove tabindex attribute
-  tab.removeAttribute("tabindex");
-
-  // Set the tab as selected
-  tab.setAttribute("aria-selected", "true");
-
-  // Get the value of aria-controls (which is an ID)
-  var controls = tab.getAttribute("aria-controls");
-
-  // Remove hidden attribute from tab panel to make it visible
-  document.getElementById(controls).removeAttribute("hidden");
-
-  // Set focus when required
-  if (setFocus) {
-    tab.focus();
-  }
-}
-
-// Deactivate all tabs and tab panels
-function deactivateTabs() {
-  for (t = 0; t < tabs.length; t++) {
-    tabs[t].setAttribute("aria-selected", "false");
-    // tabs[t].setAttribute("tabindex", "-1");
-    // tabs[t].removeEventListener("focus", focusEventHandler);
+  const data = event.target.dataset["name"];
+  switch (data) {
+    case "minus":
+      if (input.value > 0) {
+        input.value--;
+      }
+      break;
+    case "plus":
+      input.value++;
+      break;
+    case "add":
+      alert(`${input.value}개 담았다!`);
+      input.value = 0;
+      break;
+    default:
+      break;
   }
 
-  for (p = 0; p < panels.length; p++) {
-    panels[p].setAttribute("hidden", "hidden");
-  }
+  /* 
+  
+  const dataSet = (target as HTMLElementTabMenu).dataset;
+    setSelectedTabPanId(dataSet.panelId);
+    dispatch(fetchBest100(parseInt(dataSet.uniqueId, 10)));
+    */
 }
-
-
-activateTab(tabs[0]);
